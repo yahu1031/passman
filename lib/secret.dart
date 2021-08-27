67706680d60049bfda50245508d97f348a4cee56
yahu1031 <hi2minnu@gmail.com>
2021-08-27 15:22:38 +0530

    try 1

---
+++ lib/secret.dart
@@ -0,0 +1,5 @@
+const String secretPassKey = 'iRUS2gObrPHRKuv6UMw9K3Wseq0ZI08C';
+const String uNameSecretPassKey = 'eO9dVfiprxJEk0bf7HhSwYfci7IL1qcw';
+const String uPassSecretPassKey = 'sdvBo0Yyf4GEuRdeBwSvPfiohbrM5Thk';
+const String imgSecretPassKey = 'Vwra4Y9AB8PDRBr2ssh1i75BYSs4UzEW';
+const String geoEncodingAPI = 'pk.792eacd40ba5d9a0b01115fe7ba43af2';
